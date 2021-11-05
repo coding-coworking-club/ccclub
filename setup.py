2e491a7d222247dcd8c6e5c28df51605e24a4236
Dada Chan <dada.chan@ui.com>
2021-11-05 14:27:10 +0800

    Test PyPI

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='ccclub_homework_helper',
+    version='0.1',
+    license='MIT',
+    author="Dada Chan",
+    author_email='vincent8397@gmail.com',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    url='https://github.com/coding-coworking-club/ccclub',
+    keywords='ccclub homework python learning',
+    install_requires=[],
+)
